125b994fb5ca7b87c8384e8e5139142bb44b4ba0
hoorash4 <hoorash4@gmail.com>
2026-09-05 04:52:16 +0900

    Allow FSC earnings source

---
+++ supabase/migrations/20260905050000_allow_financial_services_commission_source.sql
@@ -0,0 +1,15 @@
+alter table earnings_v2.company_quarters
+  drop constraint if exists company_quarters_source_check;
+
+alter table earnings_v2.company_quarters
+  add constraint company_quarters_source_check
+  check (
+    source = any (
+      array[
+        'open_dart'::text,
+        'sec_edgar'::text,
+        'manual'::text,
+        'financial_services_commission'::text
+      ]
+    )
+  );
