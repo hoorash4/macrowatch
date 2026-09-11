e7668dcb22f7b1b89675647e3749cb070c2b6a43
hoorash4 <hoorash4@gmail.com>
2026-09-11 14:44:46 +0900

    Open economic chart workspace in current tab

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -2,6 +2,6 @@ window.MACROWATCH_CONFIG = Object.freeze({
   supabaseUrl: 'https://xhghpywvthjuvespzdul.supabase.co',
   supabasePublishableKey: 'sb_publishable_rPKY5Wfpp1JnSkPhIzJqJA_cijBqYgc',
   workspaceLinks: Object.freeze([
-    Object.freeze({ href: 'economic-charts.html', label: '경제지표 차트', iconClass: 'fa-chart-line', target: '_blank' })
+    Object.freeze({ href: 'economic-charts.html', label: '경제지표 차트', iconClass: 'fa-chart-line', target: '_self' })
   ])
 });
