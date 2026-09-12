c71b5e8591f5f665666c8d54cf2b8d4e9344558e
hoorash4 <hoorash4@gmail.com>
2026-09-13 03:05:10 +0900

    Load shared account modal on main and chart pages

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -5,3 +5,10 @@ window.MACROWATCH_CONFIG = Object.freeze({
     Object.freeze({ href: 'economic-charts.html', label: '경제지표 차트', iconClass: 'fa-chart-line', target: '_self' })
   ])
 });
+
+(() => {
+  const page = window.location.pathname.split('/').pop() || 'index.html';
+  if (!['index.html', 'economic-charts.html'].includes(page)) return;
+  if (window.MacroWatchAccountModal) return;
+  document.write('<script src="assets/js/core/account-modal.js?v=2"><\\/script>');
+})();
