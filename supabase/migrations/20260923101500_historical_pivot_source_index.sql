a1251408cf0040bfc8a79aec522568b23a40f9f2
hoorash4 <hoorash4@gmail.com>
2026-09-23 12:38:15 +0900

    Remove duplicate historical score migration filename

--- supabase/migrations/20260923101500_historical_pivot_source_index.sql
+++
@@ -1,9 +0,0 @@
-alter table public.historical_cases
-  add column if not exists pivot_source_index_code text
-  check (pivot_source_index_code in ('SP500', 'NASDAQ_COMPOSITE', 'KOSPI'));
-
-update public.historical_cases
-set pivot_source_index_code = 'NASDAQ_COMPOSITE'
-where case_code = 'tightening_2022'
-  and pivot_source_index_code is distinct from 'NASDAQ_COMPOSITE';
-
